4fc473601b8dd5baa5ea4d14f3b319549b937c56
philipantonsson <32636002+philipantonsson@users.noreply.github.com>
2018-04-13 13:17:19 +0200

    First Commit
    Commiting to the first Task

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Hur skall index.php fungera? Vilka funktioner? Se WU2_ViBo/index.php
+- Inkludera ...
+- Styra ... <-- Vad kommer den att styra?
+- Starta ... <-- Vad kommer den att starta?
+*/
+
+$cars = array("hej", "hej", "nej");
+echo " " . $cars[0] . ", " . $cars[1] . "  " . $cars[2] . ".";
+
+
+?>
+
+<?php
+$loggedIn ==false; 
+
+if ($loggedIn== true) {
+    echo"you are logged in";
+} else {
+    echo "please log in"; 
+} 
+
+?>
+
+
